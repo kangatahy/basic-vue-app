c5064db806faef656663bab40d50c85a75873c8d
Khelvin Angatahy <khellsjunior13@gmail.com>
2021-06-21 12:36:05 +0200

    Ajout du module vue-cookie

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+const VueCookie = require('vue-cookie');
+// Tell Vue to use the plugin
+Vue.use(VueCookie);
+
 Vue.config.productionTip = false
 
 new Vue({
