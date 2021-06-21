9bdb9600b3b7222657656bd50c94b05834b4a7f6
Khelvin Angatahy <khellsjunior13@gmail.com>
2021-06-21 12:35:33 +0200

    Création du composant Connection Box

---
+++ src/components/ConnectionBox.vue
@@ -0,0 +1,39 @@
+<template lang="">
+    <div>
+            <div v-if="!isConnected">
+                <input type="text" v-model="login" >
+                <input type="password" v-model="password" >
+                <input type= "button" @click="verification" value="Connexion">
+            </div>
+
+            <div v-else >
+                <h1>Salut {{login}}</h1>
+            </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ConnectionBox",
+  data() {
+    return {
+      login: "",
+      password: "",
+      isConnected: false,
+    };
+  },
+  methods: {
+    verification: function () {
+      if (this.login == "khelvin" && this.password == "khelvin") {
+        this.isConnected = true;
+        this.$cookie.set("logged-in", "true", 1);
+        console.log("je suis connecté");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="fr">
+</style>
