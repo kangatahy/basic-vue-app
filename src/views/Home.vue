7f7dfffbf51711cb24ffb0db69020392da0170d4
Khelvin Angatahy <khellsjunior13@gmail.com>
2021-06-21 12:37:24 +0200

    Remplacement de Helloworld par Connection Box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <connection-box></connection-box>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ConnectionBox from '../components/ConnectionBox.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ConnectionBox
   }
 }
 </script>
