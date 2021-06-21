20e023e06dfe420c394abd19e78c3e8b3aac39ac
Khelvin Angatahy <khellsjunior13@gmail.com>
2021-06-21 12:33:53 +0200

    création du composant datatable

---
+++ src/views/Datatable.vue
@@ -0,0 +1,16 @@
+<template lang="fr">
+    <div>
+        La Datatable
+    </div>
+</template>
+<script>
+export default {
+    name: 'Datatable',
+    components: {
+
+    },
+};
+</script>
+<style lang="">
+    
+</style>
